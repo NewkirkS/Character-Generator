80d647e71b10ae3a052e591f444cbb91b2ce297d
Caleb Stevenson and Kyle Lange <kyle@>
2016-08-29 16:50:05 -0700

    add behavior 3: Store properties for character race (1 only) in a character object (speed, languages, vision, size, special traits)

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 var abilityScoreArray = [];
 
+var CharAbilityScores = {
+  str: 0,
+  dex: 0,
+  con: 0,
+  int: 0,
+  wis: 0,
+  cha: 0
+}
+
 function Character(playerName, charName) {
   this.playerName = playerName;
   this.charName = charName;
@@ -7,15 +16,25 @@ function Character(playerName, charName) {
   this.charClass ;
   this.charLevel = 1;
   this.charSpeed = 0;
-  this.charSize = "";
   this.charHp = 0;
   this.charAc = 0;
-  this.charLanguages = [];
-  this.charAbilityScores ;
+  this.charAbilityScores = CharAbilityScores;
   this.charInit = 0;
   this.charProfBonus = 0;
   this.charSpells = [];
   this.charWeapons = [];
+}
+
+
+// ELF OBJECT
+var elf = {
+  abilityScoreIncrease: function() {
+    return newCharacter.charAbilityScores.dex += 2
+  },
+  size: "medium",
+  speed: 30,
+  languages: ["Common", "Elvish"],
+  raceTraits: ["Darkvision", "Keen Senses", "Fey Ancestry", "Trance"]
 }
 
 var rollCharAbilityScores = function(array) {
