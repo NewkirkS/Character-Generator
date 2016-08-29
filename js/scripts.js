f24ca1ac21846a96e88739a559100073dd442153
Caleb Stevenson and Kyle Lange <kyle@>
2016-08-29 12:09:52 -0700

    add behavior 1: Store all character fields for gameplay

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function Character(playerName, charName) {
+  this.playerName = playerName;
+  this.charName = charName;
+  this.charRace ;
+  this.charClass ;
+  this.charLevel = 1;
+  this.charSpeed = 0;
+  this.charSize = "";
+  this.charHp = 0;
+  this.charAc = 0;
+  this.charLanguages = [];
+  this.charAbilityScores ;
+  this.charInit = 0;
+  this.charProfBonus = 0;
+  this.charSpells = [];
+  this.charWeapons = [];
+}
