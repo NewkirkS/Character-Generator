cb038a7804ce1c6f23144c02c4b1b2922d7abbcb
Caleb Stevenson and Stephen Newkirk <caleb@>
2016-08-31 16:29:20 -0700

    add selected spells to sheet and remove some unnecessary code

--- js/scripts.js
+++ js/scripts.js
@@ -108,63 +108,6 @@ Character.prototype.abilityScoreModifier = function(obj) {
   }
 }
 
-// Weapons
-// var longbow = {
-//   name: "Longbow",
-//   type: "ranged",
-//   range: "150/600",
-//   damage: "1d8 Piercing"
-// }
-//
-// var shortsword = {
-//   name: "Shortsword",
-//   type: "melee",
-//   damage: "1d6 Piercing"
-// }
-//
-// var lightCrossbow = {
-//   name: "Light crossbow",
-//   type: "simple ranged",
-//   damage: "1d8 piercing",
-//   range: "80/320",
-//   properties: ["loading", "ammunition", "two-handed"]
-// }
-//
-// var longsword = {
-//   name: "Longsword",
-//   type: "martial melee",
-//   damage: "1d8 slashing",
-//   range: "5",
-//   properties: ["versatile (1d10)"]
-// }
-//
-// var quarterstaff = {
-//   name: "Quarterstaff",
-//   type: "simple melee",
-//   damage: "1d6 bludgeoning",
-//   range: "5",
-//   properties: ["versatile (1d8)"]
-// }
-//
-// //Armor
-// var leatherArmor = {
-//   name: "Leather Armor",
-//   armorType: "Light",
-//   ac: 1
-// }
-//
-// var chainMail = {
-//   name: "Chain Mail",
-//   armorType: "Heavy",
-//   ac: 6
-// }
-//
-// var cloth = {
-//   name: "Cloth",
-//   armorType: "Cloth",
-//   ac: 0
-// }
-
 /******
 RACES
 *******/
@@ -286,15 +229,14 @@ var cleric = {
 //UI Simulation
 // var submitTest = function(){
 //   var newCharacter = new Character("Caleb", "Thrond");
-//   newCharacter.charAbilityScores = {str: 15, dex: 12, con: 19, int: 5, wis: 7, cha: 12}
-//   newCharacter.charRace = dwarf;
+//   newCharacter.charAbilityScores = {str: 10, dex: 10, con: 10, int: 10, wis: 10, cha: 10}
+//   newCharacter.charRace = halfling;
 //   newCharacter.charClass = ranger;
 //   newCharacter.calculateStats(newCharacter);
 //   newCharacter.charAlignment = "Chaotic-Neutral";
 //   console.log(newCharacter);
 // }
 // submitTest();
-
 
 //Ability Score Roller
 var rollCharAbilityScores = function(array) {
@@ -341,10 +283,11 @@ $(document).ready(function() {
   $("#class-input").change(function(){
     var characterClass = $("#class-input").val();
     $(".class-skills").hide();
-    if (characterClass === "fighter") {
-      $("#acrobatics, #animal-handling, #athletics, #history, #insight, #intimidation, #perception, #survival").show();
-    } else if (characterClass === "ranger") {
+    $("#wizard-spells, #cleric-spells").hide();
+    if (characterClass === "ranger") {
       $("#animal-handling, #athletics, #insight, #investigation, #nature, #perception, #stealth, #survival").show();
+    } else if (characterClass === "fighter") {
+      $("#acrobatics, #animal-handling, #athletics, #history, #insight, #intimidation, #perception, #survival").show();
     } else if (characterClass === "wizard") {
       $("#arcana, #history, #insight, #investigation, #medicine, #religion").show();
       $("#wizard-spells").show();
@@ -369,9 +312,7 @@ $(document).ready(function() {
     var wisdom = parseInt($("#wisdom").val());
     var charisma = parseInt($("#charisma").val());
     var level = parseInt($("#level-input").val());
-
     var newCharacter = new Character(playName, characterName);
-
     newCharacter.charAbilityScores.str = strength;
     newCharacter.charAbilityScores.dex = dexterity;
     newCharacter.charAbilityScores.con = constitution;
@@ -379,9 +320,9 @@ $(document).ready(function() {
     newCharacter.charAbilityScores.wis = wisdom;
     newCharacter.charAbilityScores.cha = charisma;
     newCharacter.charLevel = level;
-    if (sumOfRolls !== (strength + dexterity + constitution + intelligence + wisdom + charisma)) {
-      alert("HEY!!! Please enter the exact numbers you were given!  What are you, some kind of CHEATER?!")
-    }
+    // if (sumOfRolls !== (strength + dexterity + constitution + intelligence + wisdom + charisma)) {
+    //   alert("HEY!!! Please enter the exact numbers you were given!  What are you, some kind of CHEATER?!")
+    // }
     if (race === "elf") {
       newCharacter.charRace = elf;
     } else if (race === "human") {
@@ -391,7 +332,6 @@ $(document).ready(function() {
     } else if (race === "halfling") {
       newCharacter.charRace = halfling;
     }
-
     if (characterClass === "ranger") {
       newCharacter.charClass = ranger;
     } else if (characterClass === "fighter") {
@@ -401,43 +341,39 @@ $(document).ready(function() {
     } else if (characterClass === "cleric") {
       newCharacter.charClass = cleric;
     }
-
     newCharacter.calculateStats(newCharacter);
-
     console.log(newCharacter);
 
     //output values
     $("#proficiency-bonus-sheet").text(newCharacter.charProfBonus);
     //Inspiriation needed
-
-    var skillArray = []
-    $("input:checkbox[name=skills]:checked").each (function() {
-      skillArray.push($(this).val());
-    });
-    skillArray.forEach(function(skill) {
-      $("#skills").append("<li>" + skill +  "</li>")
-    });
-    //add to list items to include ability modifier specific to skill ability
-    // " (+" + (newCharacter.charAbilityScoreModifiers.strMod + newCharacter.charProfBonus)  + ")
-
-    $("#skills").show();
-
-    $("#strength-sheet").text(newCharacter.charAbilityScores.str + " (+" + newCharacter.charAbilityScoreModifiers.strMod + ")");
-    $("#dexterity-sheet").text(newCharacter.charAbilityScores.dex + " (+" + newCharacter.charAbilityScoreModifiers.dexMod + ")");
-    $("#constitution-sheet").text(newCharacter.charAbilityScores.con + " (+" + newCharacter.charAbilityScoreModifiers.conMod + ")");
-    $("#intelligence-sheet").text(newCharacter.charAbilityScores.int + " (+" + newCharacter.charAbilityScoreModifiers.intMod + ")");
-    $("#wisdom-sheet").text(newCharacter.charAbilityScores.wis + " (+" + newCharacter.charAbilityScoreModifiers.wisMod + ")");
-    $("#charisma-sheet").text(newCharacter.charAbilityScores.cha + " (+" + newCharacter.charAbilityScoreModifiers.chaMod + ")");
+    $("#strength-sheet").text(newCharacter.charAbilityScores.str);
+    $("#dexterity-sheet").text(newCharacter.charAbilityScores.dex);
+    $("#constitution-sheet").text(newCharacter.charAbilityScores.con);
+    $("#intelligence-sheet").text(newCharacter.charAbilityScores.int);
+    $("#wisdom-sheet").text(newCharacter.charAbilityScores.wis);
+    $("#charisma-sheet").text(newCharacter.charAbilityScores.cha);
     $("#perception-sheet").text(newCharacter.charAbilityScoreModifiers.wis); //plus perception
     $("#languages-sheet").text(newCharacter.charRace.languages[0] + " and " + newCharacter.charRace.languages[1]);
     $("#ac-sheet").text(newCharacter.charAc);
-    $("#initiative-sheet").text(newCharacter.charAbilityScoreModifiers.dexMod);
+    $("#initiative-sheet").text(newCharacter.charAbilityScoreModifiers.dex);
     $("#speed-sheet").text(newCharacter.charRace.speed);
     $("#hp-sheet").text(newCharacter.charHp);
     $("#hd-sheet").text(newCharacter.charLevel + newCharacter.charClass.hitDie);
     $("#race-traits-sheet").text(newCharacter.charRace.raceTraits);
-    $("#class-features-sheet").text(newCharacter.charClass.features);
+    // $("#class-features-sheet").text(newCharacter.charClass.classFeatures);
+
+
+    var chosenSpells = [];
+    $("input:checkbox[name=spells]:checked").each(function(){
+      chosenSpells.push($(this).val());
+    });
+    chosenSpells.forEach(function(index){
+      $("#spells-sheet").append("<li>" + index + "</li>");
+    });
+
     //Display equipment and attacks in character sheet
+
     newCharacter.charClass.weapons.forEach(function(index){
       if (index.type === "simple melee" || index.type === "martial melee") {
         $("#attacks-sheet").append("<li>" + index.name + " -- <br> Attack bonus: +" + (newCharacter.charAbilityScoreModifiers.strMod + newCharacter.charProfBonus) + "<br> Damage: " + index.damage + " + " + newCharacter.charAbilityScoreModifiers.strMod + "</li>");
@@ -445,9 +381,11 @@ $(document).ready(function() {
         $("#attacks-sheet").append("<li>" + index.name + " -- <br> Attack bonus: +" + (newCharacter.charAbilityScoreModifiers.dexMod + newCharacter.charProfBonus) + "<br> Damage: " + index.damage + " + " + newCharacter.charAbilityScoreModifiers.dexMod + "<br> Range: " + index.range + " ft.</li>");
       }
     });
+
     newCharacter.charClass.weapons.forEach(function(index){
       $("#equipment-sheet").append("<li>" + index.name + "</li>");
     });
     $("#equipment-sheet").append("<li>" + newCharacter.charClass.armor.name + "</li>");
+
   });
 });
